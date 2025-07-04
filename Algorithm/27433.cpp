f9f6ab6cc3f50374a06ba82c5c19d7c2dd4eedb6
rlaRhEn <129706645+rlaRhEn@users.noreply.github.com>
2025-07-04 10:56:31 +0900

    7_04

---
+++ Algorithm/27433.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int Factorial(int n)
+{
+	if (n == 1) return 1;
+
+	return n* Factorial(n - 1);
+
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int num;
+	cin >> num;
+	
+
+	cout << Factorial(num) << '\n';
+
+	return 0;
+}
